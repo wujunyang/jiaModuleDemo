f4417659a0424b8f24eb0176ea22f99b2bcaa6bd
wujunyang <wujunyang@126.com>
2016-09-19 08:56:02 +0800

    引入Aspects用于实现AOP功能

---
+++ jiaModuleDemo/BaseModule/JiaCore/jiaBaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  jiaBaseViewController.h
+//  jiaModuleDemo
+//
+//  Created by wujunyang on 16/9/14.
+//  Copyright © 2016年 wujunyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface jiaBaseViewController : UIViewController
+
+@end
---
+++ jiaModuleDemo/BaseModule/JiaCore/jiaBaseViewController.m
@@ -0,0 +1,13 @@
+//
+//  jiaBaseViewController.m
+//  jiaModuleDemo
+//
+//  Created by wujunyang on 16/9/14.
+//  Copyright © 2016年 wujunyang. All rights reserved.
+//
+
+#import "jiaBaseViewController.h"
+
+@implementation jiaBaseViewController
+
+@end
