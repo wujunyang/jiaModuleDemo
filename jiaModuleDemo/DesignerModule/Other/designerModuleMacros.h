de21273c53852ceac96b8e3ed665c75bdc971ce1
wujunyang <wujunyang@126.com>
2016-09-20 20:06:34 +0800

    增加相关宏定义文件

---
+++ jiaModuleDemo/DesignerModule/Other/designerModuleMacros.h
@@ -0,0 +1,13 @@
+//
+//  designerModuleMacros.h
+//  jiaModuleDemo Designer模块所要用到的宏定义（这些宏定义是在公共宏定义没有时，此模块单独运用）
+//
+//  Created by wujunyang on 16/9/20.
+//  Copyright © 2016年 wujunyang. All rights reserved.
+//
+
+#ifndef designerModuleMacros_h
+#define designerModuleMacros_h
+
+
+#endif /* designerModuleMacros_h */
---
+++ jiaModuleDemo/DesignerModule/Other/designerModulePrefixHeader.h
@@ -0,0 +1,22 @@
+//
+//  designerModulePrefixHeader.h
+//  jiaModuleDemo 该模块常用的头文件引入
+//
+//  Created by wujunyang on 16/9/20.
+//  Copyright © 2016年 wujunyang. All rights reserved.
+//
+
+#ifndef designerModulePrefixHeader_h
+#define designerModulePrefixHeader_h
+
+#import "jiaMacros.h"
+#import "designerModuleMacros.h"
+#import <AFNetworking/AFNetworking.h>
+#import <Masonry/Masonry.h>
+#import <JSONModel/JSONModel.h>
+#import "UIImageView+WebCache.h"
+#import "MBProgressHUD.h"
+#import "MBProgressHUD+MP.h"
+#import "YTKNetwork.h"
+
+#endif /* designerModulePrefixHeader_h */
